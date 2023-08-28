72cd0fadebc3d88379f2f9df267468a5544aa316
zain <patniz486@gmial.com>
2023-08-28 23:42:23 +0500

    added a single child scroll view.

--- lib/add_task_list.dart
+++ lib/add_task_list.dart
@@ -14,7 +14,7 @@ class _AddTasklistState extends State<AddTasklist> {
   final repeatController = TextEditingController();
   final priorityController = TextEditingController();
   final locationController = TextEditingController();
-
+  final reminderController = TextEditingController();
   final List<Map<String, dynamic>> tasks = [];
 
   Widget buildPropertyInput(
@@ -44,89 +44,71 @@ class _AddTasklistState extends State<AddTasklist> {
         appBar: AppBar(
           title: Text('ToDo List App'),
         ),
-        body: //ListView.builder(
-            //   itemCount: tasks.length + 1,
-            //   itemBuilder: (context, index) {
-            //     if (index < tasks.length) {
-            //       final task = tasks[index];
-
-            //       return ListTile(
-            //         title: Text(task['title']),
-            //         subtitle: Column(
-            //           crossAxisAlignment: CrossAxisAlignment.start,
-            //           children: [
-            //             buildPropertyDisplay(
-            //                 Icons.calendar_today, 'Start Date', task['startDate']),
-            //             buildPropertyDisplay(
-            //                 Icons.lock_clock, 'Due Date', task['dueDate']),
-            //             buildPropertyDisplay(Icons.repeat, 'Repeat', task['repeat']),
-            //             buildPropertyDisplay(
-            //                 Icons.flag, 'is it important', task['priorityColor']),
-            //             buildPropertyDisplay(
-            //                 Icons.location_on, 'Location', task['location']),
-            //           ],
-            //         ),
-            //       );
-            //     } else {
-
-            Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              child: Text(
-                'Add a New Task',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                child: Text(
+                  'Add a New Task',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            buildPropertyInput(
-              Icons.title,
-              'Title',
-              titleController,
-            ),
-            buildPropertyInput(
-              Icons.calendar_today,
-              'Start Date',
-              startDateController,
-            ),
-            buildPropertyInput(
-              Icons.lock_clock,
-              'Due Date',
-              dueDateController,
-            ),
-            buildPropertyInput(
-              Icons.repeat,
-              'Repeat',
-              repeatController,
-            ),
-            buildPropertyInput(
-              Icons.flag,
-              'Priority',
-              priorityController,
-            ),
-            buildPropertyInput(
-              Icons.location_on,
-              'Location',
-              locationController,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: ElevatedButton(
-                onPressed: () {
-                  tasks.add({
-                    'title': titleController.text,
-                    'startDate': startDateController.text,
-                    'dueDate': dueDateController.text,
-                    'repeat': repeatController.text,
-                    'priorityColor': priorityController.text,
-                    'location': locationController.text,
-                  });
-                  setState(() {});
-                },
-                child: Text('Add Task'),
+              buildPropertyInput(
+                Icons.title,
+                'Title',
+                titleController,
               ),
-            ),
-          ],
+              buildPropertyInput(
+                Icons.calendar_today,
+                'Start Date',
+                startDateController,
+              ),
+              buildPropertyInput(
+                Icons.lock_clock,
+                'Due Date',
+                dueDateController,
+              ),
+              buildPropertyInput(
+                Icons.repeat,
+                'Repeat',
+                repeatController,
+              ),
+              buildPropertyInput(
+                Icons.flag,
+                'Priority',
+                priorityController,
+              ),
+              buildPropertyInput(
+                Icons.location_on,
+                'Location',
+                locationController,
+              ),
+              buildPropertyInput(
+                Icons.notifications,
+                'reminder',
+                reminderController,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: ElevatedButton(
+                  onPressed: () {
+                    tasks.add({
+                      'title': titleController.text,
+                      'startDate': startDateController.text,
+                      'dueDate': dueDateController.text,
+                      'repeat': repeatController.text,
+                      'priorityColor': priorityController.text,
+                      'location': locationController.text,
+                    });
+                    setState(() {});
+                  },
+                  child: Text('Add Task'),
+                ),
+              ),
+            ],
+          ),
         ));
   }
 
@@ -142,8 +124,4 @@ class _AddTasklistState extends State<AddTasklist> {
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(home: AddTasklist()));
 }
