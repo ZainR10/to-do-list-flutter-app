84c926090212ba6f880a40209562f0182516e0c4
zain <patniz486@gmial.com>
2023-08-30 23:52:51 +0500

    previous tasks not showing fixed it

--- lib/todo_initial_screen.dart
+++ lib/todo_initial_screen.dart
@@ -1,14 +1,25 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'add_task_list.dart';
 
 class AppInitialScreen extends StatefulWidget {
-  const AppInitialScreen({super.key});
+  final List<Task> tasks;
+
+  AppInitialScreen({required this.tasks});
 
   @override
-  State<AppInitialScreen> createState() => _AppInitialScreenState();
+  _AppInitialScreenState createState() => _AppInitialScreenState();
 }
 
 class _AppInitialScreenState extends State<AppInitialScreen> {
+  List<Task> tasks = []; // Initialize the tasks list in the state
+
+  @override
+  void initState() {
+    super.initState();
+    tasks = List.from(widget.tasks); // Initialize tasks from the widget
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +32,36 @@ class _AppInitialScreenState extends State<AppInitialScreen> {
           ),
         ],
       ),
-      bottomNavigationBar:
-          CurvedNavigationBar(backgroundColor: Colors.purpleAccent, items: [
-        Center(child: Icon(Icons.add)),
-      ]),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.purpleAccent,
+        items: [Center(child: Icon(Icons.add))],
+        onTap: (int index) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddTasklist(
+                addTaskCallback: _addTask,
+              ),
+            ),
+          );
+        },
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tasks[index].title),
+            subtitle: Text(tasks[index].description),
+            // Display other task details as needed
+          );
+        },
+      ),
     );
+  }
+
+  void _addTask(Task newTask) {
+    setState(() {
+      tasks.add(newTask); // Add the new task to the list
+    });
   }
 }
